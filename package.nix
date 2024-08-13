373c338d50fa185169bac225bcc28d5d7892d745
olistrik <oliverstrik@gmail.com>
2024-08-13 22:26:20 +0200

    chore: bump version to 1.6.0

--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.buildGoModule {
   name = "goskew";
-  version = "1.5.0";
+  version = "1.6.0";
   vendorHash = "sha256-83r+2dK0gHF/f2iyBfa0N7KlemOjdngFx1c1Yx00+as=";
   src = ./.;
 }
